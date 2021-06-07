a37da165837211d71b231ba528054956e7dfd8a2
loretta-arineva <wowshifter@gmail.com>
2021-06-07 10:37:53 +0800

    Added basic logo in header

--- src/pages/containers/Header.js
+++ src/pages/containers/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
+import Logo from '../components/HeaderComponents/Logo';
 import HeaderNavigation from '../components/HeaderComponents/HeaderNavigation';
 
 const Header = () => {
     return (
         <header>
+            <Logo></Logo>
             <HeaderNavigation />
         </header>
     )
