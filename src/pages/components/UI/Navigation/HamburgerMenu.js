ab3e35d386bc19ed5c019c222586a4b55449726e
loretta-arineva <wowshifter@gmail.com>
2021-06-07 10:52:58 +0800

    Rearranging project structure

---
+++ src/pages/components/UI/Navigation/HamburgerMenu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import hamburger from '../../../../assets/images/icon-hamburger.svg';
+
+const HamburgerMenu = () => {
+    return (
+        <div className="hamburger-icon">
+            <img src={hamburger} alt="Hamburger Menu Icon" />
+        </div>
+    )
+}
+
+export default HamburgerMenu
